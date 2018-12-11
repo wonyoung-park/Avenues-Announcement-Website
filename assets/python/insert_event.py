534f251619bfd5a0ef0bb26507ba67e7df5df3d0
omarASC5 <ocobas19@avenues.org>
2018-12-10 23:38:14 -0500

    Currently trying to add an event using a python script

---
+++ assets/python/insert_event.py
@@ -0,0 +1,32 @@
+# Source: http://wescpy.blogspot.com/2015/09/creating-events-in-google-calendar.html
+from __future__ import print_function
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
+
+GMT_OFF = '-05:00'      # PDT/MST/GMT-7
+EVENT = {
+    'summary': 'Dinner with friends',
+    'start':  {'dateTime': '2015-09-15T19:00:00%s' % GMT_OFF},
+    'end':    {'dateTime': '2015-09-15T22:00:00%s' % GMT_OFF},
+    'attendees': [
+        {'email': 'friend1@example.com'},
+        {'email': 'friend2@example.com'},
+    ],
+}
+
+e = GCAL.events().insert(calendarId='primary',
+        sendNotifications=True, body=EVENT).execute()
+
+print('''*** %r event added:
+    Start: %s
+    End:   %s''' % (e['summary'].encode('utf-8'),
+        e['start']['dateTime'], e['end']['dateTime']))
